bb06656116bd104fc8736eacde1086f2140cb26b
ASFAND YAR KHAN <139148529+yarkhan706@users.noreply.github.com>
2025-01-16 14:43:23 +0500

    Add files via upload

---
+++ Lab15-16CC/Lab15-16CC/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15_16CC
+{
+    internal class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string designation { get; set; }
+        public int salary { get; set; }
+    }
+}
